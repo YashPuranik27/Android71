3e9eff345073c879de10fbcd8950433ace8a2995
Yash Puranik <yap13@scarletmail.rutgers.edu>
2023-11-28 18:01:45 -0500

    "Finished" AlbumCollection.java. We may still need to make changes as we work on it. Also made small edits to other classes.

--- app/src/main/java/com/example/android71/Photo.java
+++ app/src/main/java/com/example/android71/Photo.java
@@ -6,42 +6,42 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Photo implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private String fileName;
-    private Serialized Serialized;
-    private Tag personTag, locationTag;
+	private String fileName;
+	private Persistence persistence;
+	private Tag personTag, locationTag;
 
-    public Photo(String fileName, Serialized serialized) {
-        this.fileName = fileName;
-        this.Serialized = serialized;
-        this.personTag = new Tag("person");
-        this.locationTag = new Tag("location");
-    }
+	public Photo(String fileName, Persistence persistence) {
+		this.fileName = fileName;
+		this.persistence = persistence;
+		this.personTag = new Tag("person");
+		this.locationTag = new Tag("location");
+	}
 
-    // Getters
-    public String getFileName() { return fileName; }
-    public Serialized getSerialized() { return Serialized; }
-    public Tag getPersonTag() { return personTag; }
-    public Tag getLocationTag() { return locationTag; }
+	// Getters
+	public String getFileName() { return fileName; }
+	public Persistence getBitmapSerialized() { return persistence; }
+	public Tag getPersonTag() { return personTag; }
+	public Tag getLocationTag() { return locationTag; }
 
-    // Setters
-    public void setFileName(String fileName) { this.fileName = fileName; }
-    public void setSerialized(Serialized bitmapSerialized) { this.Serialized = bitmapSerialized; }
-    public void setPersonTag(Tag personTag) { this.personTag = personTag; }
-    public void setLocationTag(Tag locationTag) { this.locationTag = locationTag; }
+	// Setters
+	public void setFileName(String fileName) { this.fileName = fileName; }
+	public void setSerialized(Persistence persistence) { this.persistence = persistence; }
+	public void setPersonTag(Tag personTag) { this.personTag = personTag; }
+	public void setLocationTag(Tag locationTag) { this.locationTag = locationTag; }
 
-    // Simplified returnAllTagValues using streams
-    public ArrayList<String> returnAllTagValues() {
-        return Stream.concat(personTag.getValues().stream(), locationTag.getValues().stream())
-                .collect(Collectors.toCollection(ArrayList::new));
-    }
+	// Simplified returnAllTagValues using streams
+	public ArrayList<String> returnAllTagValues() {
+		return Stream.concat(personTag.getValues().stream(), locationTag.getValues().stream())
+				.collect(Collectors.toCollection(ArrayList::new));
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof Photo)) return false;
-        Photo photo = (Photo) obj;
-        return fileName != null && fileName.equals(photo.fileName);
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (!(obj instanceof Photo)) return false;
+		Photo photo = (Photo) obj;
+		return fileName != null && fileName.equals(photo.fileName);
+	}
 }
--- app/src/main/java/com/example/android71/Tag.java
+++ app/src/main/java/com/example/android71/Tag.java
@@ -5,47 +5,47 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class Tag implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private String name;
-    private ArrayList<String> values = new ArrayList<>();
+	private String name;
+	private ArrayList<String> values = new ArrayList<>();
 
-    public Tag(String name) {
-        this.name = name;
-    }
+	public Tag(String name) {
+		this.name = name;
+	}
 
-    public boolean startsWithTagValue(String query) {
-        String queryLower = query.toLowerCase();
-        return values.stream().anyMatch(s -> s.toLowerCase().startsWith(queryLower));
-    }
+	public boolean startsWithTagValue(String query) {
+		String queryLower = query.toLowerCase();
+		return values.stream().anyMatch(s -> s.toLowerCase().startsWith(queryLower));
+	}
 
-    public boolean addValue(String value) {
-        if ("location".equals(name) && !values.isEmpty()) {
-            values.set(0, value);
-            return true;
-        }
-        return values.stream().noneMatch(value::equalsIgnoreCase) && values.add(value);
-    }
+	public boolean addValue(String value) {
+		if ("location".equals(name) && !values.isEmpty()) {
+			values.set(0, value);
+			return true;
+		}
+		return values.stream().noneMatch(value::equalsIgnoreCase) && values.add(value);
+	}
 
-    public void removeValue(String value) {
-        values.removeIf(value::equalsIgnoreCase);
-    }
+	public void removeValue(String value) {
+		values.removeIf(value::equalsIgnoreCase);
+	}
 
-    public String getName() { return name; }
-    public ArrayList<String> getValues() { return values; }
+	public String getName() { return name; }
+	public ArrayList<String> getValues() { return values; }
 
-    public void setName(String name) { this.name = name; }
-    public void setValues(ArrayList<String> values) { this.values = values; }
+	public void setName(String name) { this.name = name; }
+	public void setValues(ArrayList<String> values) { this.values = values; }
 
-    @Override
-    public boolean equals(Object t) {
-        if (this == t) return true;
-        if (!(t instanceof Tag)) return false;
-        Tag tag = (Tag) t;
-        return name.equals(tag.name) && values.equals(tag.values);
-    }
+	@Override
+	public boolean equals(Object t) {
+		if (this == t) return true;
+		if (!(t instanceof Tag)) return false;
+		Tag tag = (Tag) t;
+		return name.equals(tag.name) && values.equals(tag.values);
+	}
 
-    public String printTagValues() {
-        return values.stream().collect(Collectors.joining(" | "));
-    }
+	public String printTagValues() {
+		return values.stream().collect(Collectors.joining(" | "));
+	}
 }
